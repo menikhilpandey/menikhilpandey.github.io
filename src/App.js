4558442d12fdb91227b66f570f332439b9ab3bb4
Nikhil Pandey <menikhilpandey@gmail.com>
2020-01-18 11:54:15 +0530

    Full Width CSS Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends React.Component {
+  renderDesk() {
+    return (
+      <div>
+        Hello! I am a Desk!
+      </div>
+    )
+  }
+
+  renderDrawer() {
+    return (
+      <div>
+        Hello! I am a Drawer!
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.renderDesk()}
+        {this.renderDrawer()}
+      </div>
+    )
+  }
 }
 
 export default App;
