e540c8cca39e0c7bf3f6c1f7d60262f7b18b2983
Nikhil Pandey <menikhilpandey@gmail.com>
2020-01-18 15:16:19 +0530

    Menu Options Completed

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const LINKS = {
+    RESUME: "https://drive.google.com/file/d/1d_sDMmZjHu7yLa3mQjRXyk_Bo7Jej-3U/view?usp=sharing",
+    LINKEDIN: "https://www.linkedin.com/in/menikhilpandey/",
+    GITHUB: "https://github.com/menikhilpandey",
+	MEDIUM: "https://medium.com/@menikhilpandey",
+	TWITTER: "https://twitter.com/menikhilpandey"
+}
