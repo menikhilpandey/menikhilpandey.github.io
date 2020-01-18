6d922616ef506c6942099de01d15fcb15a618064
Nikhil Pandey <menikhilpandey@gmail.com>
2020-01-18 19:17:07 +0530

    Updates

---
+++ precache-manifest.388733f1858b8757cf9e1487e2910322.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdd31a12cb52809f9cb32aa9123598b",
+    "url": "/menikhilpandey.github.io/index.html"
+  },
+  {
+    "revision": "d99955390626287174d7",
+    "url": "/menikhilpandey.github.io/static/css/main.35f8c684.chunk.css"
+  },
+  {
+    "revision": "e66a865db09afea71f5d",
+    "url": "/menikhilpandey.github.io/static/js/2.f4953281.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/menikhilpandey.github.io/static/js/2.f4953281.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d99955390626287174d7",
+    "url": "/menikhilpandey.github.io/static/js/main.bfcf941c.chunk.js"
+  },
+  {
+    "revision": "0657a6bde43900549a98",
+    "url": "/menikhilpandey.github.io/static/js/runtime-main.fba991ba.js"
+  },
+  {
+    "revision": "27be4362e1b75dfa424c4aec47f6bd59",
+    "url": "/menikhilpandey.github.io/static/media/profile.27be4362.jpg"
+  }
+]);
