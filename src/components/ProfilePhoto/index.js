8c2f443df77acef363385620521ed58bb9d140bb
Nikhil Pandey <menikhilpandey@gmail.com>
2021-05-15 08:01:18 +0530

    more files
    Signed-off-by: Nikhil Pandey <menikhilpandey@gmail.com>

--- src/components/ProfilePhoto/index.js
+++ src/components/ProfilePhoto/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import './index.css'
 
-const images = {
-    profilePic: require('./assets/profile.jpeg'),
-}
-
 class ProfilePhoto extends React.PureComponent {
     render() {
         return (
             <div className={'ProfilePhotoContainer'}>
                 <img
                     className="ProfilePhoto"
-                    src={images.profilePic}
+                    src={'./profile.jpeg'}
                     alt={'Profile'}
                 />
             </div>
