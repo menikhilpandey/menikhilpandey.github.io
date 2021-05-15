74e91c9e505078b20030da63cdeb6ad03a123998
Nikhil Pandey <menikhilpandey@gmail.com>
2021-05-15 07:52:00 +0530

    added prev code
    Signed-off-by: Nikhil Pandey <menikhilpandey@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import ProfilePhoto from './components/ProfilePhoto'
+import MenuOptions from './components/MenuOptions'
+import Description from './components/Description'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {}
+interface State {
+    isNavbar: boolean
+}
+class App extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            isNavbar: false,
+        }
+    }
+    renderProfilePhoto() {
+        return <ProfilePhoto isNavbar={this.state.isNavbar} />
+    }
+
+    renderDescription() {
+        return <Description />
+    }
+
+    renderMenuOptions() {
+        return <MenuOptions isNavbar={this.state.isNavbar} />
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.renderProfilePhoto()}
+                {this.renderDescription()}
+                {this.renderMenuOptions()}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const LINKS = {
+    RESUME: 'https://drive.google.com/file/d/1d_sDMmZjHu7yLa3mQjRXyk_Bo7Jej-3U/view?usp=sharing',
+    LINKEDIN: 'https://www.linkedin.com/in/menikhilpandey/',
+    GITHUB: 'https://github.com/menikhilpandey',
+    MEDIUM: 'https://medium.com/@menikhilpandey',
+    TWITTER: 'https://twitter.com/menikhilpandey',
+    CODECHEF: 'https://codechef.com/users/menikhilpandey',
+}
